f4ae47ab229e7a1fb8dd1b9b0bbe85c0cb4ffa0b
Dennis <degameros@gmail.com>
2025-12-24 00:39:06 +0100

    changes created

--- docs/js/site.js
+++ docs/js/site.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll("section");
     const navLinks = document.querySelectorAll(".nav-item");
+    const navbarId = document.getElementById("navbarId");
     const stickyImage = document.getElementById("a_backHome");
+    const gitStick = document.getElementById("git_image");
+    const linkedListStick = document.getElementById("linked_image");
+
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry =>  {
@@ -12,17 +16,29 @@ document.addEventListener('DOMContentLoaded', () => {
                 const idValue = entry.target.id;
                 const activeLink = document.querySelector(`.nav-item[href="#${idValue}"]`);
                 
-
-                if(activeLink)
+                if(activeLink){
                     activeLink.classList.add("active");
+                }
 
-                if(entry.target.id === "sOne"){
-                    console.log("Hei der");
-                    stickyImage.hidden = true;
+                if(entry.target.id !== "sOne"){
+                    navbarId.classList.add("hidden");
+                } else {
+                    navbarId.classList.remove("hidden");
                 }
-                else {
-                    stickyImage.hidden = false;
-                }
+
+                const hideHomeButton = entry.target.id === "sOne"; //instead of if statement
+                stickyImage.hidden = hideHomeButton; 
+
+                const hideContact = entry.target.id === "sOne" || entry.target.id === "sTwo"; //instead of if statement
+                gitStick.style.transition = "opacity 3s ease-in-out";
+                gitStick.style.opacity = "1";
+                gitStick.hidden = hideContact;
+                linkedListStick.hidden = hideContact;
+                console.log(hideContact ? "Contact stick hidden" : "Contact stick show"); 
+
+                const hideNavBar = entry.target.id === "sOne"
+                navbarId.remove = hideNavBar;
+                console.log(hideNavBar ? "navbar hidden" : "Navbar not hidden");
             }
             
         });
