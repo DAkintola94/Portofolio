f92ce7cfae7537fbdc94b3fd7f1f292e4d713d69
Dennis <degameros@gmail.com>
2025-12-24 01:08:24 +0100

    transition changes

--- docs/js/site.js
+++ docs/js/site.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll("section");
     const navLinks = document.querySelectorAll(".nav-item");
     const navbarId = document.getElementById("navbarId");
-    const stickyImage = document.getElementById("a_backHome");
+    const stickyImage = document.getElementById("image_backHome");
     const gitStick = document.getElementById("git_image");
     const linkedListStick = document.getElementById("linked_image");
 
@@ -26,8 +26,11 @@ document.addEventListener('DOMContentLoaded', () => {
                     navbarId.classList.remove("hidden");
                 }
 
-                const hideHomeButton = entry.target.id === "sOne"; //instead of if statement
-                stickyImage.hidden = hideHomeButton; 
+                if(entry.target.id === "sOne"){
+                    stickyImage.classList.add("hideSticker");
+                } else {
+                    stickyImage.classList.remove("hideSticker");
+                }
 
                 const hideContact = entry.target.id === "sOne" || entry.target.id === "sTwo"; //instead of if statement
                 gitStick.style.transition = "opacity 3s ease-in-out";
@@ -35,10 +38,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 gitStick.hidden = hideContact;
                 linkedListStick.hidden = hideContact;
                 console.log(hideContact ? "Contact stick hidden" : "Contact stick show"); 
-
-                const hideNavBar = entry.target.id === "sOne"
-                navbarId.remove = hideNavBar;
-                console.log(hideNavBar ? "navbar hidden" : "Navbar not hidden");
             }
             
         });
