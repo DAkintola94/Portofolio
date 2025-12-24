c3228d8a022bd2c9edfc26c4a18348591622dd86
Dennis <degameros@gmail.com>
2025-12-25 00:15:35 +0100

    comments

--- docs/js/hamMenu.js
+++ docs/js/hamMenu.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 const hamburgerMenu = document.querySelector(".mobile_navbar");
 const offScreenMenu = document.querySelector(".off-screen-menu");
-const aTagClick = document.querySelectorAll(".off-screen-menu a");
+const aTagClick = document.querySelectorAll(".off-screen-menu a"); // this only returns a collection of element from the class 
+// (returns a NodeList, similar to array-like list of element)
+// aTagClick is not a single element, It’s a NodeList, so to need another loop later to work with each individual elements
 
 hamburgerMenu.addEventListener("click", () => {
     hamburgerMenu.classList.toggle("initiate");
     offScreenMenu.classList.toggle("initiate");
 })
 
-aTagClick.forEach(link => {
+aTagClick.forEach(link => { //Another loop again since event listeners are attached to individual elements, not to a collection
     link.addEventListener("click", () => {
         hamburgerMenu.classList.remove("initiate");
         offScreenMenu.classList.remove("initiate");
