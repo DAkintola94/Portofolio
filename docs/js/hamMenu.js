05152689f59609bdd631bdb31fb8668875e5358b
Dennis <degameros@gmail.com>
2025-12-25 01:07:07 +0100

    further comments

--- docs/js/hamMenu.js
+++ docs/js/hamMenu.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-const hamburgerMenu = document.querySelector(".mobile_navbar");
-const offScreenMenu = document.querySelector(".off-screen-menu");
-const aTagClick = document.querySelectorAll(".off-screen-menu a"); // this only returns a collection of element from the class 
+const hamburgerMenu = document.querySelector(".mobile_navbar"); //querySelector returns the first element, therefor you dont need loop to work with single items
+
+const offScreenMenu = document.querySelector(".off-screen-menu"); //querySelector returns the first element, therefor you dont need loop to work with single items
+
+const aTagClick = document.querySelectorAll(".off-screen-menu a"); // querySelectorAll returns a collection of element from the class 
 // (returns a NodeList, similar to array-like list of element)
 // aTagClick is not a single element, It’s a NodeList, so to need another loop later to work with each individual elements
 
